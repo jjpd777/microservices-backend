f4e807634c8468977fcaa42a409202d2018589f2
Juanjo <jpalacio@busbot.us>
2018-08-12 18:55:07 -0400

    Added Docker, endpoint available in container IP

--- users-service/project/__init__.py
+++ users-service/project/__init__.py
@@ -1,12 +1,15 @@
-from	flask	import	Flask,	jsonify
-#	instantiate	the	app
-app	=	Flask(__name__)
+import os
+from flask import Flask, jsonify
+# instantiate the app
+app = Flask(__name__)
 
+app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object('project.config.DevelopmentConfig')
 
-@app.route('/users/ping',	methods=['GET'])
-def	ping_pong():
-	return	jsonify({
-		'status':	'success',
-		'message':	'pong!'
-	})
+#print(app.config)
+@app.route('/users/ping', methods=['GET'])
+def ping_pong():
+ return jsonify({
+  'status': 'success',
+  'message': 'pong!'
+ })
