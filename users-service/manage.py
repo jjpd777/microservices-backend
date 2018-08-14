7baa8384ef55b0caa886269ba9d31cfd47608c46
Juanjo <jpalacio@busbot.us>
2018-08-14 14:19:11 -0400

    Database in container, and linked to users-service container

--- users-service/manage.py
+++ users-service/manage.py
@@ -1,5 +1,14 @@
-from flask_script import	Manager
-from project import	app
-manager	= Manager(app)
-if	__name__	==	'__main__':
-				manager.run()
+from flask_script import Manager
+from project import app, db
+
+manager = Manager(app)
+
+@manager.command
+def recreate_db():
+    """Recreates a database."""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+if __name__ == '__main__':
+    manager.run()
--- users-service/project/config.py
+++ users-service/project/config.py
@@ -1,14 +1,20 @@
-class	BaseConfig:
-				"""Base	configuration"""
-				DEBUG	=	False
-				TESTING	=	False
-class	DevelopmentConfig(BaseConfig):
-				"""Development	configuration"""
-				DEBUG	=	True
-class	TestingConfig(BaseConfig):
-				"""Testing	configuration"""
-				DEBUG	=	True
-				TESTING	=	True
-class	ProductionConfig(BaseConfig):
-				"""Production	configuration"""
-				DEBUG	=	False
+import os
+
+class BaseConfig:
+    """Base configuration"""
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATION = False
+class DevelopmentConfig(BaseConfig):
+    """Development configuration"""
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+class TestingConfig(BaseConfig):
+    """Testing configuration"""
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
